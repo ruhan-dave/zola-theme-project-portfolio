6c51071136e0020b428fe6f1007577f52b0711a6
Adrian Winterstein <adrian@winterstein.biz>
2024-08-17 13:49:20 +0200

    Added Dockerfile and shell script for updating the generated CSS file

---
+++ update-css.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# get the directory in which this script is located (the theme source dir)
+SOURCE_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+TAILWINDCSS_COMMAND="tailwindcss -i src/css/main.css -o static/css/main.css --minify"
+
+# call the tailwindcss cli executable, if it can be found in the path
+if command -v tailwindcss >/dev/null; then
+    pushd "${SOURCE_DIR}" >/dev/null
+    ${TAILWINDCSS_COMMAND}
+    popd >/dev/null
+
+# alternatively use docker to create and run a container and execute tailwinds cli from there
+elif command -v docker >/dev/null; then
+
+    # create the container for zola builds and tailwindcss updates
+    docker build -t develop-zola-tailwindcss "${SOURCE_DIR}" || exit 2
+
+    # update the CSS file using tailwindcss
+    docker run --user $UID:$(id -g) --mount type=bind,source=${SOURCE_DIR},target=/source -w /source develop-zola-tailwindcss \
+        ${TAILWINDCSS_COMMAND} || exit 3
+
+# print an error message if neither tailwindcss nor the docker executable could be found
+else
+    echo "Need either 'tailwindcss' or 'docker' executable in the path." >&2
+    exit 1
+fi
